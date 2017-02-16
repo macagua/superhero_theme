1eba2c23807737b22d20714e7bbf0de7340199a6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-02-16 11:37:59 -0400

    Initial Odoo 9 scaffold theme created

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+{
+    # Theme information
+    'name': "superheroe_theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "My Company",
+    'website': "",
+}
