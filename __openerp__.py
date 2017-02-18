3166127751c2f99331701fbb59a6b94a222cd20e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-02-18 11:11:36 -0400

    Added more information about this odoo theme module

--- __openerp__.py
+++ __openerp__.py
@@ -1,11 +1,13 @@
 {
     # Theme information
-    'name': "superheroe_theme",
-    'description': """ Example of a bootstrap theme moved into Odoo 9!
-    """,
+    'name': "Bootswatch Superhero Theme",
+    'summary': 'Support for Bootswatch Superhero theme',
+    'description': 'This theme module is exclusively for master theme to keep the support of Bootswatch Superhero Theme into Odoo 9.',
     'category': 'Theme',
     'version': '1.0',
     'depends': ['website'],
+    'images': ['static/description/bootswatch_superhero.png'],
+    'application': False,
 
     # templates
     'data': [
@@ -15,11 +17,12 @@
     ],
 
     # demo pages
-    'demo': [
-        'demo/pages.xml',
-    ],
+    # 'demo': [
+    #     'demo/pages.xml',
+    # ],
 
     # Your information
     'author': "Leonardo J. Caballero G.",
     'website': "https://lcaballero.wordpress.com/",
 }
+
